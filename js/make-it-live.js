f2097cef50509b6ffe5a189a8a971742166cf10d
Mike Garrett <mgarrett@webdevelopmentgroup.com>
2013-11-09 21:34:39 -0500

    JS Lint

--- js/make-it-live.js
+++ js/make-it-live.js
@@ -1,10 +1,11 @@
 function doConnect() {
+	var lastId;
 	if(window.ID) {
 		lastId = window.ID;
 	} else {
 		lastId = 0;
 	}
-	var ajaxer = $.ajax({
+	$.ajax({
 		url: "fetch.php",
 		dataType: "json",
 		data: {id: lastId, ajax: true}
@@ -12,12 +13,11 @@ function doConnect() {
 		.done( function(data) {
 			console.log(data);
 			if(data.errors) {
-				data.errors.forEach(function(element, index, array) {
+				data.errors.forEach(function(element) {
 					$("#results").prepend('<div class="error"><p><strong>ERROR</strong> '+element.message+'</p></div>');
 				});
 			} else {
 				if(data.statuses.length > 0) {
-					var status, user, last_id;
 					var x = window.statuses.length;
 
 					window.ID = data.statuses[0].id_str;
@@ -37,15 +37,15 @@ function displayTweet() {
 		var status = window.statuses.pop();
 		if(status !== undefined) {
 			if($('#'+status.id_str).length > 0) {
-				return false
+				return false;
 			}
-			user = status.user;
+			var user = status.user;
 			var content = '<div class="tweet" id="'+status.id_str+'" style="display: none;">';
 			content += '<p>'+status.text+' <br>';
 			content += '<span>&mdash;<strong>'+user.name+'</strong> <a href="http://twitter.com/'+user.screen_name+'">@'+user.screen_name+'</a></span></p>';
 			content += '<p><a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'">View Status</a> | <a href="https://twitter.com/intent/tweet?in_reply_to='+status.id_str+'">Reply to tweet</a></p>';
 			if(status.entities.media !== undefined){
-				status.entities.media.forEach(function(element, index, array) {
+				status.entities.media.forEach(function(element) {
 					if(element.type == 'photo') {
 						content += '<a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'"><img src="'+element.media_url+'" class="entities"></a>';
 					}
@@ -59,7 +59,7 @@ function displayTweet() {
 	}
 }
 function getArchives() {
-	var ajaxer = $.ajax({
+	$.ajax({
 	url: "archives.php",
 	dataType: "json",
 	data: {ajax: true}
@@ -67,24 +67,9 @@ function getArchives() {
 	.done( function(data) {
 		console.log(data);
 		if(data.statuses.length > 0) {
-			var status, user, last_id;
 			var content = '';
-
 			for (var i=0;i<data.statuses.length;i++) {
-				status = data.statuses[i];
-				user = status.user;
-				content += '<div class="tweet" id="'+status.id_str+'">';
-				content += '<p>'+status.text+' <br>';
-				content += '<span>&mdash;<strong>'+user.name+'</strong> <a href="http://twitter.com/'+user.screen_name+'">@'+user.screen_name+'</a></span></p>';
-				content += '<p><a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'">View Status</a> | <a href="https://twitter.com/intent/tweet?in_reply_to='+status.id_str+'">Reply to tweet</a></p>';
-				if(status.entities.media !== undefined){
-					status.entities.media.forEach(function(element, index, array) {
-						if(element.type == 'photo') {
-							content += '<a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'"><img src="'+element.media_url+'" class="entities"></a>';
-						}
-					});
-				}
-				content += '</div>';
+				content += formatArchives(data.statuses[i]);
 			}
 			$('<hr>'+content).appendTo('#results');
 		}
@@ -94,4 +79,22 @@ function getArchives() {
 		console.log(data);
 	});
 
+}
+function formatArchives(data) {
+	var content = '';
+	var status = data;
+	var user = status.user;
+	content += '<div class="tweet" id="'+status.id_str+'">';
+	content += '<p>'+status.text+' <br>';
+	content += '<span>&mdash;<strong>'+user.name+'</strong> <a href="http://twitter.com/'+user.screen_name+'">@'+user.screen_name+'</a></span></p>';
+	content += '<p><a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'">View Status</a> | <a href="https://twitter.com/intent/tweet?in_reply_to='+status.id_str+'">Reply to tweet</a></p>';
+	if(status.entities.media !== undefined){
+		status.entities.media.forEach(function(element) {
+			if(element.type == 'photo') {
+				content += '<a href="https://twitter.com/'+user.screen_name+'/status/'+status.id_str+'"><img src="'+element.media_url+'" class="entities"></a>';
+			}
+		});
+	}
+	content += '</div>';
+	return content;
 }
